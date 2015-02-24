748c6df287adafeaf75d878a9190b87f2343d915
Callum Macrae <callum@macr.ae>
2015-02-24 11:18:08 +0000

    Added code

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var loadLmnTask = require('lmn-gulp-tasks');
+
+gulp.task('js', loadLmnTask('browserify', {
+	src: 'index.js',
+	dest: './dest/build.js'
+}));
+
+gulp.task('default', ['js'], function () {
+	gulp.watch('index.js', ['js']);
+});
---
+++ index.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var $ = require('jquery');
+
+$(document).on('click', '[data-toggle-target]', function (e) {
+	e.preventDefault();
+
+	handleToggles($(this).data('toggle-target'));
+});
+
+function handleToggles(input) {
+	if (!$.isArray(input)) {
+		return handleToggles(input.split(' '));
+	}
+
+	$.each(input, function (i, item) {
+		item = item.split(':');
+
+		if (item.length === 1) {
+			item.unshift('toggle');
+		}
+
+		toggles[item[0]].call(null, getElement(item[1]));
+	});
+}
+
+function getElement(toggleName) {
+	return $('[data-toggle-name~="' + toggleName + '"]');
+}
+
+var toggles = module.exports = {
+	toggle: function ($element) {
+		$element.toggle();
+	},
+	hide: function ($element) {
+		$element.hide();
+	},
+	show: function ($element) {
+		$element.show();
+	}
+};
